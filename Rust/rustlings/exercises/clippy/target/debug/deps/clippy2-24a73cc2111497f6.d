2b603f27a4d7a1b1161714755e366799d8f94732
docfate111 <tdwilliamsiv@gmail.com>
2021-06-18 17:57:55 -0700

    removing unnecessary files

--- Rust/rustlings/exercises/clippy/target/debug/deps/clippy2-24a73cc2111497f6.d
+++
@@ -1,5 +0,0 @@
-/Users/thelshell/Documents/Code/rustlings/exercises/clippy/target/debug/deps/clippy2-24a73cc2111497f6.rmeta: clippy2.rs
-
-/Users/thelshell/Documents/Code/rustlings/exercises/clippy/target/debug/deps/clippy2-24a73cc2111497f6.d: clippy2.rs
-
-clippy2.rs:
